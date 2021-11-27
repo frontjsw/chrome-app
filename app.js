8a60850d1c515e50e92956ebae1e702244ade6c4
frontjsw <89008053+frontjsw@users.noreply.github.com>
2021-11-27 19:39:04 +0900

    Login form
    Login form

---
+++ app.js
@@ -0,0 +1,33 @@
+const loginForm = document.querySelector(".login_form");
+const loginInput = document.querySelector(".login_form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden"
+const USERNAME_KEY = "username"
+
+function onLoginSubmit(event){
+    const userName = loginInput.value;
+
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    localStorage.setItem(USERNAME_KEY, userName);
+    paintGreeting(); 
+}
+
+
+function paintGreeting () {
+    const username = localStorage.getItem(USERNAME_KEY);
+    greeting.innerText = `Hello ${username}`
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUserName = localStorage.getItem(USERNAME_KEY);
+
+if (savedUserName === null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreeting(); 
+}
+
+
